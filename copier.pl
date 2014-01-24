243e83997c3e7ef65ca6da68051b53e73c4f63db
Yaroslav Shevchenko <lazy.starwing@gmail.com>
2014-01-24 19:27:59 +0400

    Working version, just new regexp needed

--- copier.pl
+++ copier.pl
@@ -1,28 +1,31 @@
-﻿#!/usr/bin/perl -w
+﻿#!/usr/bin/perl
 
 use File::Find;
+use File::Path;
 
-%files = (
-	$home_dir => "~/ftp_mirror";
-	$ftp_dir => "/mnt/ftp";
-)
+$ftp_dir = "/var/log";
+$home_dir = "/root/ftp_mirror/";
 
-find \&copier, $files{$ftp_dir};
-#find ({wanted => \&copier, no_chdir => 1}, $ftp_dir);
+find ( \&copier, $ftp_dir );
 
 sub copier {
+	$inbox_dir = "$home_dir";
+
 	# Проверить, что имя файла совпадает с шаблоном. Если да,
-	if $_ =~ /(?:[1-2]\d{3}[0-1]\d[0-3]\d\.[0-2]\d[0-6]\d[0-6]\d\.)([a-z]{4})\.20(1|2)\.\w+\.(lccs.xz|lkks)/ {
+	if ( /[1-2]\d{3}[0-1]\d[0-3]\d\.[0-2]\d[0-6]\d[0-6]\d\.[a-z]{4})\.20(1|2)\.\w+\.(lccs.xz|lkks)/ ) {
 		# Получаем список каталогов.
-		my @dirs = shift split "/", $File::Find:dir;
-		my $inbox_dir = "$files{$home_dir}/";
-		# проверить, что локально есть каталог верхнего уровня, затем второго и т.д.
+		@dirs = split (/\//, $File::Find::dir);
+		shift @dirs;
+
+		# Создаем переменную с каталогом-целью.
 		foreach my $dir (@dirs){
 			$inbox_dir = $inbox_dir . $dir . "/";
-			# Если каталога нет, создать его и продолжить проверять наличие каталогов на FTP
-			opendir my LOCAL_DIR, $inbox_dir || mkdir $inbox_dir 0777;
 		}
+
+		# Если каталога нет, создать его и продолжить проверять наличие каталогов на FTP
+		mkpath $inbox_dir unless ( -d $inbox_dir ) ; 
+
 		# Затем создать файл с таким же именем локально.
-		`touch $inbox_dir$_`;
+		open NEWF, ">>", ("$inbox_dir"."$_");
 	}
 }
