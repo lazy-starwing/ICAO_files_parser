75c53e594af7068989ae85980e4aa3971136377d
Yaroslav Shevchenko <paladininshadow@gmail.com>
2014-01-29 15:55:57 +0300

    New version almost ready

--- sorter.pl
+++ sorter.pl
@@ -18,7 +18,7 @@ open LOGFILE, ">>", $logfile;
 finddepth ( \&dig, $ftp_root );
 
 sub dig {
-	if ( /(?:20)?([0-9][0-9][0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:([A-Z]{4}|CCC)\.(20)?(1|2)\.\w+\.|\w+\.\2.(20)?(1|2))\.(?:lccs\.xz|lkks|tar\.gz)/ ) {
+	if ( /(?:20)?([0-9][0-9][0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:([A-Z]{4}|CCC)\.(20)?(1|2)\.\w+\.|\w+\.\2.(20)?(1|2))\.(?:lccs\.xz|lkks|tar\.gz$)/ ) {
 		my $target_dir = "$ftp_root/$2/" . 20 . "$1" . "/";
 		if ( $File::Find::dir -eq $ftp_root ) {
 			say LOGFILE "File $_ with ICAO $2 was send to root ftp directory" ;
@@ -30,7 +30,15 @@ sub dig {
 			say LOGFILE "Create dir $target_dir for data file $_" ;
 		}
 		move ("$File::Find::name", "$target_dir");
-	} else
-}
+	} else {
+		if ( -f $_ && $File::Find:dir -eq "$ftp_root/m\([A-Z]{4}|CCC)\/.*" && $_ -neq ".*iploading$" ) {
+			move ($File::Find::name, "$ftp_root/$_" . "__from_" . split (/\//, $File::Find::dir));
+			say LOGFILE "Файл $File::Find::name перенесен в корень FTP" ;
+		}
+		if ( -d $_ && $Find::Find::dir =~ m/$ftp_root\/([A-Z]{4}|CCC)\/.*/ ) {
+			# Проверить, что папка пуста
+				# Удалить папку
+		}
+	}
 
 close LOGFILE;
