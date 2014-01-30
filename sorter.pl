8d9fe4db74b78e3a2f6b09da65f79505599830ce
Yaroslav Shevchenko <lazy.starwing@gmail.com>
2014-01-30 20:32:12 +0400

    With some error - move unmoved files

--- sorter.pl
+++ sorter.pl
@@ -6,38 +6,40 @@ use File::Copy;
 use File::Path qw(make_path);
 
 # корневая папка FTP.
-$ftp_root = "/var/ftp";
+$ftp_root = "/home/yar/ftp/mnt/ftp";
 # $logfile - имя файла, в который ведется логирование.
 $logfile = "/var/log/sorter.log";
-# права на каталоги на FTP-сервере.
-$permissions = "0755";
 
 # Открываем лог.
 open LOGFILE, ">>", $logfile;
 
 finddepth ( \&dig, $ftp_root );
 
+close LOGFILE;
+
 sub dig {
 	if ( /(?:20)?([0-9][0-9])[0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:\w+\.)?([A-Z]{4}|CCC)\.(?:20)?(?:1|2)\.(?:\w+\.)?(?:(?:lccs\.xz$)|(?:lkks$)|(?:tar\.gz$))/ ) {
 		my $target_dir = "$ftp_root/$2/" . 20 . "$1" . "/";
-		if ( $File::Find::dir -eq $ftp_root ) {
-			say LOGFILE "File $_ with ICAO $2 was send to root ftp directory" ;
-		} elsif ( $File::Find::dir -eq $target_dir) {
+		say LOGFILE "\nFor dir $File::Find::dir and target $target_dir"
+		if ( $File::Find::dir eq $ftp_root ) {
+			say LOGFILE "File $Find::File::name with ICAO $2 and year $1 was send to root ftp directory" ;
+		} elsif ( $File::Find::dir eq $target_dir) {
 			return;
 		}
 		unless ( -d $target_dir ) {
 			make_path ( $target_dir );
-			say LOGFILE "Create dir $target_dir for data file $_" ;
+			say LOGFILE "Create dir $target_dir for data file $File::Find::name" ;
 		}
 		move ("$File::Find::name", "$target_dir");
+		say LOGFILE "Файл $File::Find::name перемещен в $target_dir";
 	} else {
-		if ( -f $_ && $File::Find:dir -eq "$ftp_root/m\([A-Z]{4}|CCC)\/.*" && $_ -neq ".*iploading$" ) {
+		if ( -f $_ && $File::Find::dir eq <$ftp_root/([A-Z]{4}|CCC)/*> && $_ ne <*.uploading> ) {
 			move ($File::Find::name, "$ftp_root/$_" . "__from_" . split (/\//, $File::Find::dir));
 			say LOGFILE "Файл $File::Find::name перенесен в корень FTP" ;
 		}
-		if ( -d $_ && $Find::Find::dir =~ m/$ftp_root\/([A-Z]{4}|CCC)\/.*/ ) {
+		if ( -d $_ && $Find::Find::dir =~ m/^$ftp_root\/([A-Z]{4}|CCC)\/.*/ ) {
 			rmdir;
+			say LOGFILE "Каталог $File::Find::name удален";
 		}
 	}
-
-close LOGFILE;
+}
