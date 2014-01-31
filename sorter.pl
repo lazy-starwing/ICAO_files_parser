823cb345fff2f31a02fef99f56b021f31033f75f
Yaroslav Shevchenko <paladininshadow@gmail.com>
2014-01-31 15:31:56 +0300

    With comments

--- sorter.pl
+++ sorter.pl
@@ -20,27 +20,35 @@ say LOGFILE "Work done!";
 close LOGFILE;
 
 sub dig {
+	# Если файл с данными из региона
 	if ( /(?:20)?([0-9][0-9])[0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:\w+\.)?([A-Z]{4}|CCC)\.(?:20)?(?:1|2)\.(?:\w+\.)?(?:(?:lccs\.xz$)|(?:lkks$)|(?:tar\.xz$))/ ) {
 		my $target_dir = "$ftp_root/$2/" . 20 . "$1" . "/";
-		say LOGFILE "\nFor dir $File::Find::dir and target $target_dir" ;
+		# Если файл с данными лежит в корне ftp
 		if ( $File::Find::dir eq $ftp_root ) {
 			say LOGFILE "File $Find::File::name with ICAO $2 and year $1 was send to root ftp directory" ;
+		# Проверка, что файл уже в нужном каталоге
 		} elsif ( $File::Find::dir . "/" eq $target_dir) {
 			return;
 		}
+		# Если нет нужного каталога
 		unless ( -d $target_dir ) {
+			# Создаем его
 			make_path ( $target_dir );
 			say LOGFILE "Create dir $target_dir for data file $File::Find::name" ;
 		}
+		# Переместить в нужный каталог.
 		move ("$File::Find::name", "$target_dir");
 		say LOGFILE "Файл $File::Find::name перемещен в $target_dir";
+	# Заливающиеся файлы не трогать!
 	} elsif ( $_ eq <*.uploading> ) {
 		return;
 	} else {
+		# Несовпадающие файлы из папок ИКАО/год в корень перекинуть
 		if ( -f $_ && $File::Find::dir eq <$ftp_root/([A-Z]{4}|CCC)/*> ) {
 			move ($File::Find::name, "$ftp_root/$_" . "__from_" . split (/\//, $File::Find::dir));
 			say LOGFILE "Файл $File::Find::name перенесен в корень FTP" ;
 		}
+		# Удаляем пустые папки
 		if ( -d $_ && $Find::Find::dir eq <$ftp_root\([A-Z]{4}|CCC)/*> ) {
 			rmdir;
 			say LOGFILE "Каталог $File::Find::name удален";
