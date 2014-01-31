bf7a6085ffef8600979b2b6ca81719a771f9afe2
Yaroslav Shevchenko <lazy.starwing@gmail.com>
2014-01-31 16:25:50 +0400

    Final. Just comments will be.

--- sorter.pl
+++ sorter.pl
@@ -15,15 +15,17 @@ open LOGFILE, ">>", $logfile;
 
 finddepth ( \&dig, $ftp_root );
 
+say LOGFILE "Work done!";
+
 close LOGFILE;
 
 sub dig {
-	if ( /(?:20)?([0-9][0-9])[0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:\w+\.)?([A-Z]{4}|CCC)\.(?:20)?(?:1|2)\.(?:\w+\.)?(?:(?:lccs\.xz$)|(?:lkks$)|(?:tar\.gz$))/ ) {
+	if ( /(?:20)?([0-9][0-9])[0-1][0-9][0-3][0-9]\.[0-2][0-9][0-6][0-9][0-6][0-9]\.(?:\w+\.)?([A-Z]{4}|CCC)\.(?:20)?(?:1|2)\.(?:\w+\.)?(?:(?:lccs\.xz$)|(?:lkks$)|(?:tar\.xz$))/ ) {
 		my $target_dir = "$ftp_root/$2/" . 20 . "$1" . "/";
-		say LOGFILE "\nFor dir $File::Find::dir and target $target_dir"
+		say LOGFILE "\nFor dir $File::Find::dir and target $target_dir" ;
 		if ( $File::Find::dir eq $ftp_root ) {
 			say LOGFILE "File $Find::File::name with ICAO $2 and year $1 was send to root ftp directory" ;
-		} elsif ( $File::Find::dir eq $target_dir) {
+		} elsif ( $File::Find::dir . "/" eq $target_dir) {
 			return;
 		}
 		unless ( -d $target_dir ) {
@@ -32,12 +34,14 @@ sub dig {
 		}
 		move ("$File::Find::name", "$target_dir");
 		say LOGFILE "Файл $File::Find::name перемещен в $target_dir";
+	} elsif ( $_ eq <*.uploading> ) {
+		return;
 	} else {
-		if ( -f $_ && $File::Find::dir eq <$ftp_root/([A-Z]{4}|CCC)/*> && $_ ne <*.uploading> ) {
+		if ( -f $_ && $File::Find::dir eq <$ftp_root/([A-Z]{4}|CCC)/*> ) {
 			move ($File::Find::name, "$ftp_root/$_" . "__from_" . split (/\//, $File::Find::dir));
 			say LOGFILE "Файл $File::Find::name перенесен в корень FTP" ;
 		}
-		if ( -d $_ && $Find::Find::dir =~ m/^$ftp_root\/([A-Z]{4}|CCC)\/.*/ ) {
+		if ( -d $_ && $Find::Find::dir eq <$ftp_root\([A-Z]{4}|CCC)/*> ) {
 			rmdir;
 			say LOGFILE "Каталог $File::Find::name удален";
 		}
